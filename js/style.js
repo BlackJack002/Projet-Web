c3b28a0f88eafc85fa23b0f6deee966b5e6e6017
BlackJack002 <adra_s@outlook.fr>
2024-06-03 14:22:56 +0200

    Mon projet web pour le 2 juin

---
+++ js/style.js
@@ -0,0 +1,51 @@
+
+/*
+js pour menu
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.navLien');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('show');
+    });
+});
+
+
+/** js pour le bouton de positionnement */
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const scrollTopBtn = document.getElementById('scrollTopBtn');
+    const scrollBottomBtn = document.getElementById('scrollBottomBtn');
+
+    scrollTopBtn.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+
+    scrollBottomBtn.addEventListener('click', () => {
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+/** js pour activer est desactiver lien en rouge une fois cliquer */
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.navLien a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function() {
+            // Retirer la classe "active" de tous les liens
+            links.forEach(lnk => lnk.classList.remove('active'));
+            // Ajouter la classe "active" au lien cliqué
+            this.classList.add('active');
+        });
+    });
+});
